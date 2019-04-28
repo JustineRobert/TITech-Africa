6e5482ec34c6067f4b1926325a852c6d8653ff2b
Igune Justine Robert <justine.igune@stud.kiu.ac.ug>
2019-04-28 13:25:32 +0300

    cafemenu.php
    Cafe Menu

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Café Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="menu-wrapper container">
+    <h1 class="logo">Café Progate</h1>
+    <div class="menu-items">
+      <!-- Write the foreach loop -->
+      <?php foreach ($menus as $menu): ?>
+        <h3><?php echo $menu->name ?></h3>
+      <?php endforeach ?>
+      
+    </div>
+  </div>
+</body>
+</html>
+
+
+
+
+
+
 <?php
 class Menu {
   public $name;
@@ -19,3 +46,5 @@ $pasta = new Menu('PASTA');
 $menus = array($juice, $coffee, $curry, $pasta);
 
 ?>
+
+
